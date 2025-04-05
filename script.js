0b88be7bbbd63b08fc6f61d78ffe95304ab5e5cf
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-04-05 16:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function getHelloWorld() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+getHelloWorld().then((message) => {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = message;
+});
+function getHelloWorld() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+getHelloWorld().then((message) => {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = message;
+});
